57dc4fbdbc0667ae707eed2111fa80b0a5c74b62
Sombhartigoswami <sombharatigoswami@gmail.com>
2025-08-19 16:22:48 +0530

    Add files via upload

---
+++ productData.js
@@ -0,0 +1,48 @@
+let product = 
+[
+    {   id: '001',
+        product_img: 'images/hoopimg.webp',
+        company_name:'joker & witch',
+        product_name:'hoop earings',
+        price:399,
+        old_price:499,
+        discount:20.04,
+        return_period:'14',
+        delivery_date:"20 aug 2025"
+    
+    },
+    {   id: '002',
+        product_img: 'images/dressesImg.jpg',
+        company_name:' max',
+        product_name:'maxi  drees',
+        price:499,
+        old_price: 599,
+        discount:20.04,
+        return_period:'14',
+        delivery_date:"20 aug 2025"
+    
+    },
+    {   id: '003',
+        product_img: 'images/shoesimg.jpg',
+        company_name:'Redtape',
+        product_name:'women sneakers',
+        price:1199,
+        old_price:1900,
+        discount:36,
+        return_period:'14',
+        delivery_date:"20 aug 2025"
+    
+    },
+    {   id: '004',
+        product_img: 'images/heelimg.webp',
+        company_name:'Dressbery',
+        product_name:'women heel',
+        price:799,
+        old_price: 899,
+        discount:20.04,
+        return_period:'14',
+        delivery_date:"20 aug 2025"
+    
+    }
+    
+]
